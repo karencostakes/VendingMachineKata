b864777a195cfa970e524303f781777dea8873ac
Karen Costakes <karencostakes@gmail.com>
2016-09-26 18:46:05 -0400

    added junit testing for dime

--- Testing/Coin.java
+++ Testing/Coin.java
@@ -5,9 +5,12 @@ public class Coin {
 
 	public String insertCoinIntoVending(double diam, double weight) {
 		// TODO Auto-generated method stub
-		if ((diam == .84 && weight == 5.00) || (diam == .71 && weight == 2.27) || (diam == .96 && weight == 5.67)  )
+		if (diam == .84 && weight == 5.00) 
 			return "5";
-	
+		if (diam == .71 && weight == 2.27) 
+			return "10";
+		if (diam == .96 && weight == 5.67)  
+			return "25";
 		
 		return "We do not accept pennies or coins other than dimes, nickles, and quarters";
 	}
--- Testing/JUnitTestCase1.java
+++ Testing/JUnitTestCase1.java
@@ -7,13 +7,22 @@ import org.junit.Test;
 public class JUnitTestCase1 {
 
 	@Test
-	public void whenValidCoinInsertedMachineStoresCoin() {
+	public void whenNickleIsInsertedMachineStoresCoin() {
 		Coin coin1 = new Coin ();
 		assertEquals("5", coin1.insertCoinIntoVending(.84, 5.00));
 		
 	
 	}
 
+	
+	@Test
+	public void whenDimeIsInsertedMachineStoresCoin() {
+		Coin coin1 = new Coin ();
+		assertEquals("10", coin1.insertCoinIntoVending(.71, 2.27));
+		
+	
+	}
+	
 	@Test
 	public void whenInValidCoinInsertedMachineRejectsCoin() {
 		Coin coin1 = new Coin ();
